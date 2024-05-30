f7d63e4248d89d31ceee53196cc61338fad94e78
Robinson1906 <110904910+Robinson1906@users.noreply.github.com>
2024-05-29 23:18:16 -0500

    Creacion tabla Autorizaciones y diseño boton de panico

--- src/app/views/servicesAdmin/autorizaciones/autorizaciones.component.ts
+++ src/app/views/servicesAdmin/autorizaciones/autorizaciones.component.ts
@@ -1,12 +1,12 @@
-// autorizaciones.component.ts
-
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-autorizaciones',
   templateUrl: './autorizaciones.component.html',
-  standalone:true,
-  styleUrls: ['./autorizaciones.component.scss']
+  styleUrls: ['./autorizaciones.component.scss'],
+  standalone: true, // Define el componente como independiente
+  imports:[NgFor],
 })
 export class AutorizacionesComponent {
   autorizaciones = [
@@ -14,4 +14,8 @@ export class AutorizacionesComponent {
     { id: 2, name: 'María García', email: 'maria.garcia@example.com', profession: 'Médico', rentalReason: 'Turismo', rentalStartDate: '2024-06-05', rentalEndDate: '2024-06-15', carModel: 'Ford' },
     { id: 3, name: 'Carlos López', email: 'carlos.lopez@example.com', profession: 'Arquitecto', rentalReason: 'Otro', rentalStartDate: '2024-06-10', rentalEndDate: '2024-06-20', carModel: 'Honda' }
   ];
+
+  getAutorizaciones() {
+    return this.autorizaciones;
+  }
 }
